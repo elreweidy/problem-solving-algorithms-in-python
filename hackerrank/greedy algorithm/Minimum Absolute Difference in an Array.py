a192269abdcf4a031ee141bda8223f77d55d814d
ahmed el-reweidy <64925287+elreweidy@users.noreply.github.com>
2021-08-11 14:26:51 +0200

    Add files via upload

---
+++ hackerrank/greedy algorithm/Minimum Absolute Difference in an Array.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'minimumAbsoluteDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def minimumAbsoluteDifference(arr):
+    solus = []
+    sorted_arr = sorted(arr)
+    for i in range(len(arr) - 1):
+        solus.append(abs(sorted_arr[i] - sorted_arr[i + 1]))
+    return min(solus)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = minimumAbsoluteDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
